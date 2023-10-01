e06edfb1380efd37120eb0f216a7c9587223d05f
Daninuro <daninuro96@gmail.com>
2023-10-01 21:23:42 +0200

    Añadimos Ejercicio 3

---
+++ src/main/ComVariasLineas.java
@@ -0,0 +1,18 @@
+package main;
+
+public class ComVariasLineas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*System.out.println("Hola");
+		System.out.println("Adios");
+		System.out.println("Cucu");*/
+
+		for(int i=0;i<6;i++)
+			System.out.println(i+" ");
+		System.out.println("FIN DE PROGRAMA");
+		
+	}
+
+}
